28bf68b445e8920746ad42ba4a4acef82615e23e
morfi02 <morfijorge489@gmail.com>
2025-02-27 09:30:58 +0000

    diseño pagina inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,54 @@
 @section('title', 'MealMatch - Comida Casera')
 
 @section('content')
-    <h1>pagina inical</h1>
+    <!-- Sección Hero -->
+    <section class="bg-cover bg-center h-screen text-white flex items-center"
+        style="background-image: url('/images/hero-bg.jpg');">
+        <div class="container mx-auto text-center">
+            <h1 class="text-5xl font-bold">Comida casera al alcance de un clic</h1>
+            <p class="text-xl mt-4">Encuentra cocineros locales que preparan deliciosos platillos para ti.</p>
+            <a href="{{ url('/') }}"
+                class="mt-6 inline-block bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600">Ver Cocineros</a>
+        </div>
+    </section>
+     <!-- Buscador  -->
+     <section class="flex justify-center my-4">
+        <input type="text" placeholder="Ingresa tu ubicación..."
+            class="border border-gray-300 px-4 py-2 w-1/3 rounded-l-md focus:outline-none">
+        <button class="bg-green-700 text-white px-6 py-2 rounded-r-md hover:bg-amber-800">
+            Buscar
+        </button>
+    </section>
+     
 
+     <section class="text-center my-12">
+        <h3 class="text-2xl font-semibold text-gray-800">Únete a nuestra comunidad culinaria</h3>
+        <p class="text-gray-600">
+            Ya sea que quieras disfrutar de deliciosas comidas o compartir tus creaciones, tenemos algo para ti.
+        </p>
+    </section>
+
+    <!-- Opciones -->
+    <div class="flex justify-center space-x-8 my-12">
+        <div class="bg-green-700 text-white p-8 rounded-lg w-1/3 text-center shadow-md">
+            <h4 class="text-2xl font-bold">Para Amantes de la Cocina</h4>
+            <p class="mt-2">Descubre sabores únicos y apoya a cocineros locales.</p>
+            <a href="{{ url('/') }}"
+                class="inline-block mt-4 bg-white text-green-700 px-4 py-2 rounded-md hover:bg-gray-100">
+                Explorar Menús
+            </a>
+
+        </div>
+
+        <div class="bg-pink-700 text-white p-8 rounded-lg w-1/3 text-center shadow-md">
+            <h4 class="text-2xl font-bold">Para Cocineros Apasionados </h4>
+            <p class="mt-2">Comparte tu talento y gana cocinando lo que amas y aprecias.</p>
+            <a href="{{ url('/') }}"
+                class="inline-block mt-4 bg-white text-pink-700 px-4 py-2 rounded-md hover:bg-gray-100">
+                Empezar a Cocinar
+            </a>
+
+        </div>
+    </div>
 
 @endsection
